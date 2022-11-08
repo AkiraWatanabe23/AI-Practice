b81b394639202400e121e2a8c5eec9e2d5b01f19
Akira Watanabe <a0323w@gmail.com>
2022-11-08 10:57:28 +0900

    RunAwayAIのステートを管理するenum

--- Assets/Script/NavMesh/RunAway.cs
+++ Assets/Script/NavMesh/RunAway.cs
@@ -18,10 +18,12 @@ public class RunAway : MonoBehaviour
     private readonly List<GameObject> _checkPoint = new();
     /// <summary> AIの逃げる先のPositionが格納されたListのインデックス </summary>
     private int _listNum = 0;
-    /// <summary> Playerから逃げる距離にいるかどうかのフラグ </summary>
-    private bool _runAwayCheck = false;
     /// <summary> 移動時間 </summary>
     private float _time = 0.0f;
+    /// <summary> Playerから逃げる距離にいるかどうかのフラグ </summary>
+    private bool _runAwayCheck = false;
+    /// <summary> AIの行動管理 </summary>
+    private RunAwayState _state = RunAwayState.Wait;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +37,7 @@ public class RunAway : MonoBehaviour
         }
         //最初に逃げる先を指定
         _listNum = Random.Range(0, 10);
+        _state = RunAwayState.Wait;
     }
 
     // Update is called once per frame
@@ -43,9 +46,15 @@ public class RunAway : MonoBehaviour
         //Playerと、このオブジェクトの距離をとる(Vector3.Distanceは重くなると考えたので、2乗の値で比較する)
         //Vector3.SqrMagnitudeだと、2乗の値が返ってくる
         float dis = Vector3.SqrMagnitude(_player.position - transform.position);
+        //以下の書き方でも、SqrMagnitudeとやっていることは同じ
+        //float disX = _player.position.x - transform.position.x;
+        //float disY = _player.position.y - transform.position.y;
+        //float disZ = _player.position.z - transform.position.z;
+        //float dis = disX * disX + disY * disY + disZ * disZ;
 
         //Playerとこのオブジェクトとの距離(の2乗)が一定より短くなったら
-        if (dis <= _runAwayDis * _runAwayDis)
+        //以下の条件式が正しく動かない時がある(移動中に以下の条件を満たした時?)
+        if (dis <= _runAwayDis * _runAwayDis && _state == RunAwayState.Wait)
         {
             _runAwayCheck = true;
         }
@@ -53,15 +62,18 @@ public class RunAway : MonoBehaviour
         //AIにあるPositionを移動先として指定する
         if (_runAwayCheck == true)
         {
+            _state = RunAwayState.RunAway;
             _agent.SetDestination
                 (_checkPoint[_listNum].transform.position);
             _time += Time.deltaTime;
             //一定時間経ったら移動を終了する
             if (_time > 2.0f)
             {
+                _state = RunAwayState.Wait;
                 _runAwayCheck = false;
-                _listNum = Random.Range(0, 10);
                 _time = 0.0f;
+                //次の移動先を決定する
+                _listNum = Random.Range(0, 10);
             }
         }
     }
